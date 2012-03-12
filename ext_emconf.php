98127f0895608d0372f6581888b468aab3eaba0e
lwinkler <lwinkler@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-12 21:50:51 +0000

    - chg permissions and update emconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cas_reservation/trunk@59168 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cas_reservation".
 #
-# Auto generated 15-11-2009 15:50
+# Auto generated 12-03-2012 22:48
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"64e1";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"b6d7";s:20:"doc/wizard_form.html";s:4:"7bb0";}',
+	'_md5_values_when_last_written' => 'a:50:{s:9:"ChangeLog";s:4:"dcea";s:10:"README.txt";s:4:"f012";s:18:"cas_reservation.js";s:4:"a02d";s:19:"ext_emconf.php.good";s:4:"4fdf";s:12:"ext_icon.gif";s:4:"d79c";s:17:"ext_localconf.php";s:4:"481f";s:14:"ext_tables.php";s:4:"4ba8";s:14:"ext_tables.sql";s:4:"6959";s:25:"ext_tables_static+adt.sql";s:4:"ebea";s:19:"fill_dates_group.py";s:4:"aeb2";s:21:"fill_dates_special.py";s:4:"f25f";s:15:"flexform_ds.xml";s:4:"c7f7";s:16:"locallang_db.xml";s:4:"8f65";s:19:"doc/wizard_form.dat";s:4:"f2ef";s:20:"doc/wizard_form.html";s:4:"5d68";s:25:"images/entete-facture.png";s:4:"a297";s:21:"images/helpbubble.gif";s:4:"7877";s:16:"images/room1.png";s:4:"91d9";s:16:"images/room2.png";s:4:"bcda";s:16:"images/room3.png";s:4:"570a";s:41:"pibook/class.tx_casreservation_pibook.php";s:4:"9ef0";s:20:"pibook/locallang.xml";s:4:"adba";s:25:"pibook/template_book.html";s:4:"5c10";s:47:"pidisplay/class.tx_casreservation_pidisplay.php";s:4:"eb8a";s:23:"pidisplay/locallang.xml";s:4:"d37c";s:31:"pidisplay/template_display.html";s:4:"8328";s:39:"pilib/class.tx_casreservation_pilib.php";s:4:"1907";s:19:"pilib/locallang.xml";s:4:"de41";s:45:"pimanage/class.tx_casreservation_pimanage.php";s:4:"b62c";s:28:"pimanage/foot_bill_room1.txt";s:4:"dadf";s:28:"pimanage/foot_bill_room2.txt";s:4:"d235";s:28:"pimanage/foot_mail_room1.txt";s:4:"19b0";s:28:"pimanage/foot_mail_room2.txt";s:4:"44ab";s:28:"pimanage/head_bill_room1.txt";s:4:"af4c";s:28:"pimanage/head_bill_room2.txt";s:4:"0ba2";s:28:"pimanage/head_mail_room1.txt";s:4:"588d";s:28:"pimanage/head_mail_room2.txt";s:4:"99ce";s:28:"pimanage/line_bill_room1.txt";s:4:"6a13";s:28:"pimanage/line_bill_room2.txt";s:4:"ae03";s:28:"pimanage/line_mail_room1.txt";s:4:"2170";s:28:"pimanage/line_mail_room2.txt";s:4:"2170";s:22:"pimanage/locallang.xml";s:4:"68de";s:29:"pimanage/template_manage.html";s:4:"fa7d";s:34:"pimanage/plots/plot_week_view1.php";s:4:"36d5";s:34:"pimanage/plots/plot_week_view2.php";s:4:"63e5";s:34:"pimanage/plots/plot_year_view1.php";s:4:"4e96";s:34:"pimanage/plots/plot_year_view2.php";s:4:"29b7";s:20:"static/constants.txt";s:4:"80ae";s:16:"static/setup.txt";s:4:"c1b9";s:30:"static/css/cas_reservation.css";s:4:"fba9";}',
 );
 
 ?>
